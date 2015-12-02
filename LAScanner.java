4c954a38dfb324edd70a7861a822b2641566e9a1
Dylan Jager-Kujawa <dylanleekujawa@gmail.com>
2015-12-02 16:35:28 -0600

    Initial setup of java files (comments, class declarations, etc...)

---
+++ LAScanner.java
@@ -0,0 +1,22 @@
+/*
+
+Dylan Jager-Kujawa
+Matt Richardson
+
+Dr. Strader
+CSC 442
+8 December 2015
+
+Blocks World
+
+LAScanner.java
+Implements the scanner, which reads input character by character and outputs a list of 
+tokens as defined by the grammar in the handout.
+
+*/
+
+import java.util.*;
+
+public class LAScanner {
+
+}
---
+++ Project2.java
@@ -0,0 +1,22 @@
+/*
+
+Dylan Jager-Kujawa
+Matt Richardson
+
+Dr. Strader
+CSC 442
+8 December 2015
+
+Blocks World
+
+Project2.java
+
+*/
+
+import java.io.*;
+
+public class Project2 {
+	public static void main(String[]args) {
+		
+	}
+}
